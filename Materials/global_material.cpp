ec1a5e46dfcceb64472d68835f36498516e045cc
Patrick <pc799@bath.ac.uk>
2023-12-02 20:44:51 +0000

    RayTracer Code - Ken Cameron

---
+++ Materials/global_material.cpp
@@ -0,0 +1,119 @@
+/****************************************************************************
+*
+* krt - Ken's Raytracer - Coursework Edition. (C) Copyright 1993-2022.
+*
+* I've put a lot of time and effort into this code. For the last decade
+* it's been used to introduce hundreds of students at multiple universities
+* to raytracing. It forms the basis of your coursework but you are free
+* to continue using/developing forever more. However, I ask that you don't
+* share the code or your derivitive versions publicly. In order to continue
+* to be used for coursework and in particular assessment it's important that
+* versions containing solutions are not searchable on the web or easy to
+* download.
+*
+* If you want to show off your programming ability, instead of releasing
+* the code, consider generating an incredible image and explaining how you
+* produced it.
+*/
+
+#include "global_material.h"
+
+#include <math.h>
+
+GlobalMaterial::GlobalMaterial(Environment* p_env, Scene* p_sce, Colour p_reflect_weight, Colour p_refract_weight, float p_ior)
+: environment(p_env), scene(p_sce), reflect_weight(p_reflect_weight), refract_weight(p_refract_weight), ior(p_ior)
+{
+
+}
+
+Vector reflect(Vector incident, Vector& normal) {
+
+    return incident - 2 * (incident.dot(normal)) * normal;
+}
+
+bool refract(Vector& incident, Vector& normal, float ior, Vector& refractDir) {
+    float cosi = -incident.dot(normal);
+    float etai = 1, etat = ior;
+    Vector n = normal;
+    if (cosi < 0) {
+        cosi = -cosi;
+    } else {
+        std::swap(etai, etat);
+        n= -normal;
+    }
+    float eta = etai / etat;
+    float k = 1 - eta * eta * (1 - cosi * cosi);
+    if (k < 0) {
+        return true; // Total internal reflection
+    } else {
+        refractDir = eta * incident + (eta * cosi - sqrtf(k)) * n;
+        return false;
+    }
+}
+
+float computeFresnel(Vector& incident, Vector& normal, float ior) {
+    float cosi = incident.dot(normal);
+    float etai = 1, etat = ior;
+    if (cosi > 0) { std::swap(etai, etat); }
+    float sint = etai / etat * sqrtf(std::max(0.f, 1 - cosi * cosi));
+    if (sint >= 1) {
+        return 1; // Total internal reflection
+    } else {
+        float cost = sqrtf(std::max(0.f, 1 - sint * sint));
+        cosi = fabsf(cosi);
+        float Rs = ((etat * cosi) - (etai * cost)) / ((etat * cosi) + (etai * cost));
+        float Rp = ((etai * cosi) - (etat * cost)) / ((etai * cosi) + (etat * cost));
+        return (Rs * Rs + Rp * Rp) / 2;
+    }
+}
+
+
+// reflection and recursion computation goes here
+Colour GlobalMaterial::compute_once(Ray& viewer, Hit& hit, int recurse)
+{
+    if (recurse <= 0) return Colour(); // Recursion limit
+
+    // Calculate reflected ray
+    Vector reflectDir = reflect(viewer.direction, hit.normal);
+    Ray reflectedRay(hit.position + 0.0001f * reflectDir, reflectDir);
+    Colour reflectedColor;
+    float reflectedDepth;
+    if (scene != nullptr) {
+        scene->raytrace(reflectedRay, recurse - 1, reflectedColor, reflectedDepth);
+    } else {
+        std::cout << "Didn't work babes " << recurse << std::endl;
+    }
+
+    // Calculate refracted ray
+    Vector refractDir;
+    refractDir.normalise();
+    bool tir = refract(viewer.direction, hit.normal, ior, refractDir);
+    Colour refractedColor;
+    float refractedDepth;
+    if (!tir) {
+        Ray refractRay (hit.position + 0.0001f * refractDir, refractDir);
+        scene->raytrace(refractRay, recurse - 1, refractedColor, refractedDepth);
+    }
+
+    // Compute Fresnel effect
+    float fresnelEffect = computeFresnel(viewer.direction, hit.normal, ior);
+
+    // Combine reflected and refracted colors based on Fresnel effect
+    Colour finalColor;
+    finalColor.r = fresnelEffect * reflectedColor.r + (1 - fresnelEffect) * (tir ? 0 : refractedColor.r);
+    finalColor.g = fresnelEffect * reflectedColor.g + (1 - fresnelEffect) * (tir ? 0 : refractedColor.g);
+    finalColor.b = fresnelEffect * reflectedColor.b + (1 - fresnelEffect) * (tir ? 0 : refractedColor.b);
+    finalColor.a = 1.0f; // Assuming alpha is always 1, adjust if needed
+
+    return finalColor;
+
+}
+
+Colour GlobalMaterial::compute_per_light(Vector& viewer, Hit& hit, Vector& ldir)
+{
+	Colour result;
+
+
+	return result;
+}
+
---
+++ main_lab34.cpp
@@ -0,0 +1,159 @@
+/****************************************************************************
+ *
+ * krt - Ken's Raytracer - Coursework Edition. (C) Copyright 1993-2022.
+ *
+ * I've put a lot of time and effort into this code. For the last decade
+ * it's been used to introduce hundreds of students at multiple universities
+ * to raytracing. It forms the basis of your coursework but you are free
+ * to continue using/developing forever more. However, I ask that you don't
+ * share the code or your derivitive versions publicly. In order to continue
+ * to be used for coursework and in particular assessment it's important that
+ * versions containing solutions are not searchable on the web or easy to
+ * download.
+ *
+ * If you want to show off your programming ability, instead of releasing
+ * the code, consider generating an incredible image and explaining how you
+ * produced it.
+ */
+
+/* This is the top level for the program you need to create for lab three and four.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+// these are core raytracing classes
+#include "framebuffer.h"
+#include "scene.h"
+#include "material_colour.h"
+
+// classes that contain our objects to be rendered, all derived from Object
+#include "polymesh_object.h"
+#include "sphere_object.h"
+#include "plane_object.h"
+
+// classes that contain our lights, all derived from Light
+#include "directional_light.h"
+
+// classes that contain the materials applied to an object, all derived from Material
+#include "phong_material.h"
+#include "falsecolour_material.h"
+#include "global_material.h"
+
+//classes that contain cameras, all derived from Camera
+#include "simple_camera.h"
+#include "full_camera.h"
+
+using namespace std;
+
+// you will find it useful during development/debugging to create multiple functions that fill out the scene.
+void build_scene(Scene& scene)
+{
+	// The following transform allows 4D homogeneous coordinates to be transformed.It moves the supplied teapot model to somewhere visible.
+	Transform * transform = new Transform(1.0f, 0.0f, 0.0f, 0.0f,
+											0.0f, 0.0f, 1.0f, -60.0f,
+											0.0f, 1.0f, 0.0f, 20.0f,
+											0.0f, 0.0f, 0.0f, 1.0f);
+
+	//  Read in the teapot model.
+	PolyMesh* pm = new PolyMesh((char *)"teapot-low.obj", true);
+	pm->apply_transform(*transform);
+
+	Sphere* sphere = new Sphere(Vertex(-5.0f, 5.0f, 10.0f), 1.5f);
+
+	DirectionalLight* dl = new DirectionalLight(Vector(0.0f, 0.0f, 1.0f), Colour(1.0f, 1.0f, 1.0f, 0.0f));
+
+	scene.add_light(dl);
+
+    Environment* environment = new Environment();
+
+    float ior = 15.0f;
+    Colour reflectWeight (0.1f, 0.1f, 0.1f);
+    Colour refractWeight (0.9f, 0.9f, 0.9f);
+
+    GlobalMaterial* gm1 = new GlobalMaterial(environment, &scene, reflectWeight, refractWeight, ior);
+
+	Phong* bp1 = new Phong(new MaterialColour(0.0f, 0.1f, 0.0f,1.0f), new MaterialColour(0.0f, 0.4f, 0.0f,1.0f), new MaterialColour(0.4f, 0.4f, 0.4f,1.0f), 40.f);
+	Phong* bp2 = new Phong(new MaterialColour(0.1f, 0.1f, 0.1f,1.0f), new MaterialColour(0.5f, 0.0f, 0.0f,1.0f), new MaterialColour(0.4f, 0.4f, 0.4f,1.0f), 40.f);
+
+	Phong *whiteMaterial = new Phong(new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f),
+									 new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f),
+									 new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f), 40.f);
+	Phong *greenMaterial = new Phong(new MaterialColour(0.0f, 1.0f, 0.0f, 1.0f),
+									 new MaterialColour(0.0f, 0.5f, 0.0f, 1.0f),
+									 new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f), 40.f);
+	Phong *redMaterial = new Phong(new MaterialColour(1.0f, 0.0f, 0.0f, 1.0f),
+								   new MaterialColour(0.5f, 0.0f, 0.0f, 1.0f),
+								   new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f), 40.f);
+	Phong *blueMaterial = new Phong(new MaterialColour(0.0f, 0.0f, 0.5f, 1.0f),
+									new MaterialColour(0.0f, 0.0f, 0.5f, 1.0f),
+									new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f), 40.f);
+
+
+	pm->set_material(bp1);
+
+	scene.add_object(pm);
+
+	sphere->set_material(gm1);
+
+	scene.add_object(sphere);
+
+	Plane *ceiling = new Plane(0.0f, 1.0f, 0.0f, -9.0f);
+	Plane *floor = new Plane(0.0f, -1.0f, 0.0f, -14.0f);
+	Plane *backWall = new Plane(0.0f, 0.0f, 1.0f, -25.0f);
+	Plane *leftWall = new Plane(1.0f, 0.0f, 0.0f, -20.0f);
+	Plane *rightWall = new Plane(-1.0f, 0.0f, 0.0f, -20.0f);
+
+	ceiling->set_material(whiteMaterial);
+	floor ->set_material(whiteMaterial);
+	backWall->set_material(blueMaterial);
+	leftWall->set_material(redMaterial);
+	rightWall->set_material(greenMaterial);
+
+	scene.add_object(ceiling);
+	scene.add_object(floor);
+	scene.add_object(backWall);
+	scene.add_object(leftWall);
+	scene.add_object(rightWall);
+}
+
+
+// This is the entry point function to the program.
+int main(int argc, char *argv[])
+{
+	int width = 512;
+	int height = 512;
+	// Create a framebuffer
+	FrameBuffer* fb = new FrameBuffer(width, height);
+	
+	// Create a scene
+	Scene scene;
+	
+	// Setup the scene
+	build_scene(scene);
+	
+	// Declare a camera
+	// Camera *camera = new SimpleCamera(0.5f);
+
+	Vertex cameraPosition (0.0f, -3.0f, -5.0f);
+	Vector cameraLookAT (0.0f, 0.0f, 1.0f);
+	Vector cameraUP (0.0f, 1.0f, 0.0f);
+
+	Camera* camera = new FullCamera(0.5f, cameraPosition, cameraLookAT, cameraUP);
+	// Camera* camera = new FullCamera(0.5f, Vertex(-1.0f, 0.0f, 0.0f), Vector(1.0f, 0.0f, 1.0f), Vector(0.0f, 1.0f, 0.0f));
+	
+	// Camera generates rays for each pixel in the framebuffer and records colour + depth.
+	camera->render(scene,*fb);
+	
+	// Output the framebuffer colour and depth as two images
+	fb->writeRGBFile((char *)"test.ppm");
+	fb->writeDepthFile((char *)"depth.ppm");
+	
+	cerr << "\nDone.\n" << flush;
+	
+	// a debug check to make sure we don't leak hits.
+	cerr << "Hit Pool " << Hit::pool_size << " Allocated " << Hit::allocated << "\n" << flush;
+	return 0;
+}
